312d8d206f51d674c59c448a1dcb4ff77d281707
Martin Landsiedel <martin.landsiedel@continental-corporation.com>
2022-10-29 23:08:45 +0200

    Unternehmen Admin Seite fertig Keine Fehler mehr

--- admin/index.php
+++ admin/index.php
@@ -1,5 +1,10 @@
 <?php
 
+/* Parameters ------------------------------------------- */
+$code_cost = 1;                                             // Cost of one code
+$disziplinen = array('Laufen', 'Inliner', 'Schwimmen', 'Radfahren');
+/* Parameters end */
+
 session_start();
 // Connect to database
 $db_host_name = 'db5010643227.hosting-data.io';
@@ -15,6 +20,8 @@ $pdo = new PDO("mysql:host=$db_host_name;dbname=$database",
 /* Login required --------------------------------------- */
 // Print login form
 function login_form($title) {
+    global $pdo;
+    
     echo "<html>
             <head>
                 <title>Unternehmensportal: Login</title> 
@@ -36,20 +43,22 @@ function login_form($title) {
 
 /* Check login ------------------------------------------ */
 function login() {
+    global $pdo;
+
     if(!isset($_POST['username']) || !isset($_POST['password'])) {
         login_form("Fehler bei der Datenübertragung.<br>Anmeldung erforderlich");
         return false;
     }
-    $statement = $pdo->prepare("SELECT * FROM Company WHERE `login` = :`login`");
+    $statement = $pdo->prepare("SELECT * FROM Company WHERE `login` = :login");
     $statement->execute(array(':login' => $_POST['username']));
-    if($statement->rowCount() <= 0) {
+    $rows = $statement->fetchAll();                         // There should only be a single row for this username
+    if(count($rows) <= 0) {
         login_form("Anmeldeinformationen sind falsch.<br>Anmeldung erforderlich");
         return false;
     }
-    $row = $statement->fetch()[0];                          // There should only be a single row for this username
-    if(password_verify($_POST['password'], $row['password'])) { // Login successful
+    if(password_verify($_POST['password'], $rows[0]['password'])) { // Login successful
         $_SESSION['valid'] = true;
-        $_SESSION['companyId'] = $row['companyId'];
+        $_SESSION['companyId'] = $rows[0]['companyId'];
         return true;
     }
     login_form("Anmeldeinformationen sind falsch.<br>Anmeldung erforderlich");
@@ -58,35 +67,234 @@ function login() {
 
 /* Logout ----------------------------------------------- */
 function logout() {
+    global $pdo;
+    
     $_SESSION['valid'] = false;
     session_destroy();
 }
 
 
 /* Code management ====================================== */
+/* Purchase new codes form ------------------------------ */
+function codes_new_form() {
+    global $pdo;
+    
+    echo "<html>
+            <head>
+                <title>Unternehmensportal: Neue Codes generieren</title> 
+            </head>
+            <body>
+                <h2>Neue Codes erwerben</h2>
+                <form method='POST' action='index.php?a=new_codes_checkout'>
+                    <label for='number'>Anzahl Codes: </label>
+                    <input type='number' name='number'>
+                    <br>
+                    <label for='users'>Mitarbeiter Namen (Format: Nachname,Vorname;): </label>
+                    <!--<input type='file' name='users' accept='text'>-->
+                    <textarea name='users' rows='20' cols='100'>
+                        Nachname, Vorname;
+                        Nachname, Vorname;
+                    </textarea>
+                    <br><br>
+                    <input type='submit' value='Kostenpflichtig kaufen'>
+                </form>
+            </body>
+        </html>";
+}
+
+/* Purchase new codes ----------------------------------- */
+function codes_new_checkout() {
+    global $pdo;
+    
+//    if(!isset($_FILES['users']) || !isset($_POST['number'])) {
+    if(!isset($_POST['users']) || !isset($_POST['number'])) {
+        echo "<h2>Fehler bei der Datenübertragung</h2>
+        <a href='index.php?a=new_codes'>Erneut versuchen</a>";
+        return false;
+    }
+    // TODO: Rechnung verschicken
+    // Codes generieren, ggf neue Nutzer anlegen
+    //$handle = fopen($_FILES['users']['tmp_name'], "r");
+    //if ($handle) {                                          // Read new users line by line from file
+        $count = 0;
+        $statement = $pdo->prepare("SELECT userId FROM User WHERE `name` = ? AND `surname` = ?;");
+        $uinsstatement = $pdo->prepare("INSERT INTO User (`name`, `surname`, `compID`)
+                                        VALUES (:name, :surname, :compId)");
+        $cinsstatement = $pdo->prepare("INSERT INTO Codes (`userId`, `enabled`)
+                                        VALUES (:userId, :enabled)");
+        $lines = explode(';', $_POST['users']);
+        //while (($line = fgets($handle)) !== false && $count < $_POST['number']) {
+        foreach($lines as $line) {
+            $name = explode(',', $line, 2);
+            $statement->execute(array(trim($name[1]), trim($name[0])));
+            if($statement->rowCount() > 0) {                // User exists -> only create code
+                $userid = $statement->fetch()['userId'];
+            }
+            else {                                          // User does not exist -> create user and code
+                $uinsstatement->execute(array('name' => trim($name[1]), 'surname' => trim($name[0]), 'compId' => $_SESSION['companyId']));
+                $userid = $pdo->lastInsertId();
+            }
+            $cinsstatement->execute(array("userId" => $userid, "enabled" => 1)); // Codes are enabled as they are already paid
+        }
+    //    fclose($handle);
+    //}
+
+    // Liste der aktuellen Codes anzeigen
+    codes_list();
+}
+
+/* List all codes --------------------------------------- */
+function codes_list() {
+    global $pdo;
+    
+    echo "<html>
+            <head>
+                <title>Unternehmensportal: Ihre Codes</title>
+            </head>
+            <body>
+                <h2>Codes, die Ihrem Unternehmen zugeordnet sind</h2>
+                <table border='1'>
+                    <tr>
+                        <th>Code</th>
+                        <th>Mitarbeiter</th>
+                        <th>Enabled</th>
+                    </tr>";
+    $statement = $pdo->prepare("SELECT Codes.code, User.name, User.surname, Codes.enabled FROM Codes INNER JOIN User ON Codes.userId = User.userId
+                                WHERE User.compID = ? ORDER BY Codes.enabled;");
+    $statement->execute(array($_SESSION['companyId']));
+    while($row = $statement->fetch()) {
+        echo "<tr>
+                <td>";
+        echo $row['code'];
+        echo   "</td>
+                <td>";
+        echo $row['name'] . " " . $row['surname'];
+        echo   "</td>
+                <td>";
+        echo $row['enabled'];
+        echo   "</td>
+            </tr>";
+        $comp_id = $row['companyId'];
+        $comp_name = $row['name'];
+        echo "<option value='$comp_id'>$comp_name</option>";
+    }
+    echo "      </table>
+            </body>
+        </html>";
+}
 
 
 /* Event management ===================================== */
 /* Register new event form ------------------------------ */
 function event_new_form() {
+    global $pdo, $disziplinen;
+    
     echo "<html>
             <head>
                 <title>Unternehmensportal: Neues Event</title> 
             </head>
             <body>
                 <h2>Neues Event anlegen</h2>
-                <form method='POST' action='register_event.php'>
+                <form method='POST' action='index.php?a=new_event_reg'>
                     <label for='name'>Event Name: </label>
                     <input type='text' name='username'>
                     <br>
-                    <label for='description'>Event Beschreibung: </label>
-                    <textarea name='description' rows='20' cols='100'>
+                    <label for='desc'>Event Beschreibung: </label>
+                    <textarea name='desc' rows='20' cols='100'>
+                        Enter event description here
+                    </textarea>
+                    <br>
+                    <label for='disziplinen'>Priorisierte Disziplin auswählen: </label>
+                    <select name='disziplin'>";
+    foreach($disziplinen as $disziplin) {
+        $disziplin_lower = strtolower($disziplin);
+        echo "<option value='$disziplin_lower'>$disziplin</option>";
+    }
+    echo "          </select>
+                    <br>
+                    <label for='firmen'>Partizipierende Firmen einladen (mehrere möglich): </label>
+                    <select name='firmen[]' multiple>";
+    $statement = $pdo->prepare("SELECT `companyId`, `name` FROM Company WHERE companyId <> ?");
+    $statement->execute(array($_SESSION['companyId']));
+    $rows = $statement->fetchAll();                         // There should only be a single row for this username
+    foreach($rows as $row) {
+        $comp_id = $row['companyId'];
+        $comp_name = $row['name'];
+        echo "<option value='$comp_id'>$comp_name</option>";
+    }
+    echo "          </select>
                     <br><br>
                     <input type='submit' value='Anlegen'>
                 </form>
             </body>
         </html>";
 }
+
+/* Register new event ----------------------------------- */
+function event_new() {
+    global $pdo;
+
+    // Parameter prüfen
+    if(!isset($_POST['name']) || !isset($_POST['desc'])
+       || !isset($_POST['disziplinen']) | !isset($_POST['firmen'])) {
+        echo "<h2>Fehler bei der Datenübertragung</h2>
+        <a href='index.php?a=event_new'>Erneut versuchen</a>";
+        return false;
+    }
+    // Neues Event registrieren, Firmen hinzufügen
+    $event = array();
+    $event['name'] = $_POST['name'];
+    $event['description'] = $_POST['desc'];
+    $event['disziplin'] = $_POST['disziplin'];
+    $event['initiatorComp'] = $_SESSION['companyId'];
+    $statement = $pdo->prepare("INSERT INTO Events (`name`, `description`, disciplines, initiatorComp)
+                                VALUES (:name, :description, :disziplinen, :initiatorComp)");
+    $statement->execute($event);
+
+    $event_id = $pdo->lastInsertId();
+    $eventcomp = array();
+    $eventcomp['eventId'] = $event_id;
+    $statement = $pdo->prepare("INSERT INTO EventCompanies (eventId, compId, agreed)
+                                VALUES (:eventId, compId, 0)");
+    $statement->execute($_SESSION['companyId']);            // Include company itself
+    foreach($_POST['firmen'] as $firma) {
+        $eventcomp['compId'] = $firma;                      // Ein Eintrag pro Firma
+        $statement->execute($eventcomp);
+    }
+    return true;
+}
+
+/* List own events -------------------------------------- */
+function event_list() {
+    global $pdo;
+    
+    // List every event where the Company is listed and the company agreed
+    $statement = $pdo->prepare("SELECT Events.* FROM Events INNER JOIN EventCompanies ON Events.eventid = EventCompanies.eventid
+                                WHERE EventCompanies.agreed = 1 AND EventCompanies.compId = ?;");
+    $statement->execute(array($_SESSION['companyId']));
+    while($row = $statement->fetch()) {
+        echo "<div style='border: 1px solid black'>
+                <span>Name: " . $row['name'] . "</span>
+                <span>Beschreibung: " . $row['description'] . "</span>
+                <span>Bevorzugte Disziplin: " . $row['disciplines'] . "</span>
+                <span>Beteiligte Firmen: ";
+                $statement = $pdo->prepare("SELECT Company.*, EventCompanies.agreed FROM EventCompanies INNER JOIN Company ON Company.companyId = EventCompanies.compId
+                                            WHERE EventCompanies.eventId = ?;");
+                $statement->execute($row['eventId']);
+                while($row2 = $statement->fetch()) {
+                    if($row2['agreed']) {
+                        $agreed = 'agreed';
+                    }
+                    else {
+                        $agreed = 'no answer';
+                    }
+                    echo $row2['name'] . " (" . $agreed ."), ";
+                }
+        echo   "</span>
+            </div>";
+    }
+}
+
 
 
 /* MAIN ================================================= */
@@ -109,16 +317,34 @@ if(!isset($_SESSION['valid']) || $_SESSION['valid'] !== true) {
 
 // Switch actions
 if(isset($_GET['a'])) {
+    $exit = true;
     switch ($_GET['a']) {
         case 'new_event':
-            
+            event_new_form();
+            break;
+        case 'new_event_reg':
+            if(event_new() === true) {
+                $exit = false;                              // Nicht beenden, Events anzeigen
+            }
+            break;
+        case 'list_events':
+            event_list();
             break;
         case 'new_codes':
-
+            codes_new_form();
+            break;
+        case 'new_codes_checkout':
+            codes_new_checkout();
+            break;
+        case 'list_codes':
+            codes_list();
             break;
         default:
-            // Ignore
+            $exit = false;
             break;
+    }
+    if($exit) {
+        exit(0);
     }
 }
 
@@ -135,5 +361,12 @@ if(isset($_GET['a'])) {
         <a href="index.php?a=new_event">Neues Event initiieren</a>
         <br>
         <a href="index.php?a=new_codes">Neue Zugangscodes generieren</a>
+        <br>
+        <a href="index.php?a=list_codes">Zugangscodes anzeigen</a>
+        <br><br>
+        <h3>Von Ihnen gestartete Events</h3>
+            <?php
+                event_list();
+            ?>
     </body>
 </html>
