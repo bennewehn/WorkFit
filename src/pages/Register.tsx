d1bae1a0789dfaced897b355b9cd4a5de1488906
Benedikt Wehner <benedikt.wehner@outlook.de>
2022-10-29 21:18:48 +0200

    validaion functionality added

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,7 +1,7 @@
 import { IonContent, IonIcon } from "@ionic/react";
 import { IonPage } from "@ionic/react";
-import { useColorMode, Input, InputGroup, InputRightElement, Button, Container, Stack, FormControl, FormLabel, Flex, Icon } from '@chakra-ui/react'
-import { useState } from "react";
+import { useColorMode, Input, InputGroup, InputRightElement, Button, Container, Stack, FormControl, FormLabel, Flex, Icon, FormHelperText } from '@chakra-ui/react'
+import React, { useState } from "react";
 import { eyedrop, eyeOffOutline, eyeOutline } from "ionicons/icons";
 
 const Register: React.FC = () => {
@@ -13,39 +13,113 @@ const Register: React.FC = () => {
     const [password, setPassword] = useState('')
     const [code, setCode] = useState('')
 
+    const errorInit = {
+        password: '', email: '', name: '', surname: '', code: ''
+    };
+
+    const [errors, setErrors] = useState(errorInit);
+
     const colorMode = useColorMode();
 
-    const isValid = false
+    const validateEmail = (email: String) => {
+        return String(email)
+            .toLowerCase()
+            .match(
+                /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+            );
+    };
+
+    const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        if (e.target.value.length < 6) {
+            setErrors({ ...errors, password: "Passwort muss min. 6 Zeichen lang sein." })
+        }
+        else {
+            setErrors({ ...errors, password: "" })
+        }
+        setPassword(e.target.value)
+    }
+
+    const handleEMailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        if (e.target.value === "") {
+            setErrors({ ...errors, email: "E-Mail Adresse wird benötigt." })
+        }
+        else if (!validateEmail(e.target.value)) {
+            setErrors({ ...errors, email: "Falsche E-Mail Adresse." })
+        }
+        else {
+            setErrors({ ...errors, email: "" })
+        }
+        setMail(e.target.value)
+    }
+
+    const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        if (e.target.value === "") {
+            setErrors({ ...errors, name: "Vorname wird benötigt." })
+        }
+        else {
+            setErrors({ ...errors, name: "" })
+        }
+        setName(e.target.value)
+    }
+
+    const handleSurname = (e: React.ChangeEvent<HTMLInputElement>) => {
+        if (e.target.value === "") {
+            setErrors({ ...errors, surname: "Nachname wird benötigt." })
+        }
+        else {
+            setErrors({ ...errors, surname: "" })
+        }
+        setSurname(e.target.value)
+    }
+
+    const handleCodeChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        if (e.target.value === "") {
+            setErrors({ ...errors, code: "Code wird benötigt." })
+        }
+        else {
+            setErrors({ ...errors, code: "" })
+        }
+        setCode(e.target.value)
+    }
+
+    const handleRegisterClick = () => {
+
+    }
+
 
     return (
         <IonPage>
             <IonContent>
                 <Flex direction="column" justifyContent="center" height={"100%"} maxWidth={"40rem"} margin={"auto"}>
-                    <Stack  m={10}>
-                        <img style={{ marginBottom: "5vh"}} src={colorMode.colorMode === "light" ? "assets/images/logo_with_text.png" : "assets/images/logo_with_text_white.png"}></img>
+                    <Stack m={10}>
+                        <img style={{ marginBottom: "5vh" }} src={colorMode.colorMode === "light" ? "assets/images/logo_with_text.png" : "assets/images/logo_with_text_white.png"}></img>
                         <Stack spacing={3} paddingInline={"5%"}>
-                            <FormControl>
+                            <FormControl isRequired>
                                 <FormLabel>E-Mail</FormLabel>
-                                <Input placeholder="test@test.com" onChange={event => setMail(event.target.value)} />
+                                <Input placeholder="max.mustermann@gmail.com" onChange={handleEMailChange} />
+                                <FormHelperText color={"red.500"}>{errors.email}</FormHelperText>
                             </FormControl>
-                            <FormControl>
+                            <FormControl isRequired>
                                 <FormLabel>Vorname</FormLabel>
-                                <Input onChange={event => setName(event.target.value)} />
+                                <Input placeholder="Max" onChange={handleNameChange} />
+                                <FormHelperText color={"red.500"}>{errors.name}</FormHelperText>
                             </FormControl>
-                            <FormControl>
+                            <FormControl isRequired>
                                 <FormLabel>Nachname</FormLabel>
-                                <Input onChange={event => setSurname(event.target.value)} />
+                                <Input placeholder="Mustermann" onChange={handleSurname} />
+                                <FormHelperText color={"red.500"}>{errors.surname}</FormHelperText>
                             </FormControl>
-                            <FormControl>
+                            <FormControl isRequired >
                                 <FormLabel>Code</FormLabel>
-                                <Input onChange={event => setCode(event.target.value)} />
+                                <Input onChange={handleCodeChange} />
+                                <FormHelperText color={"red.500"}>{errors.code}</FormHelperText>
                             </FormControl>
-                            <FormControl>
+                            <FormControl isInvalid={errors.password !== ""} isRequired>
                                 <FormLabel>Passwort</FormLabel>
                                 <InputGroup>
                                     <Input
                                         type={show ? 'text' : 'password'}
-                                        onChange={event => setPassword(event.target.value)}
+                                        onChange={handlePasswordChange}
                                     />
                                     <InputRightElement width='3rem'>
                                         <Button h='1.75rem' size='sm' onClick={handleClick}>
@@ -53,12 +127,11 @@ const Register: React.FC = () => {
                                         </Button>
                                     </InputRightElement>
                                 </InputGroup>
+                                <FormHelperText color={"red.500"}>{errors.password}</FormHelperText>
                             </FormControl>
-
-                        <Button>Registrieren</Button>
+                            <Button onClick={handleRegisterClick} isDisabled={Object.values(errors).filter(e => e!=="").length !== 0}>Registrieren</Button>
                         </Stack>
                     </Stack>
-
                 </Flex>
             </IonContent>
         </IonPage>
