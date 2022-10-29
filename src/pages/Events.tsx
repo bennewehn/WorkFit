d5c8f9b8ebd05910176e21128b6e3d3cf9320d20
AntonPieper <anton.pieper@informatik.hs-fulda.de>
2022-10-29 16:32:28 +0200

    Update

--- src/pages/Events.tsx
+++ src/pages/Events.tsx
@@ -1,25 +1,18 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Events.css';
+import {IonContent, IonPage} from "@ionic/react";
+import styles from "./Events.module.css";
+import {Input} from "@chakra-ui/react";
 
 const Events: React.FC = () => {
-  return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Events</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Events</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 2 page" />
-      </IonContent>
-    </IonPage>
-  );
+    return (
+        <IonPage>
+            <IonContent fullscreen>
+                <div>
+                    <img className={styles.logo} src="assets/images/logo.png" alt="Logo"/>
+                    <Input placeholder="Search"/>
+                </div>
+            </IonContent>
+        </IonPage>
+    );
 };
 
 export default Events;
