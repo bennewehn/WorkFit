1eadb425be76acda867218a21d87a4b92db7fe78
Benedikt Wehner <benedikt.wehner@outlook.de>
2022-10-30 08:01:40 +0100

    leaderboard fetch data

--- src/pages/Loaderboard.jsx
+++ src/pages/Loaderboard.jsx
@@ -1,60 +1,34 @@
 import { IonContent, IonPage } from '@ionic/react';
+import { useContext, useEffect, useState } from 'react';
 import LoaderboardUser from '../components/LeaderBoardUser';
-
+import { TokenContext } from '../loginContext'
 const Loaderboard = () => {
 
-  const usersFake = [
-    {
-      userId: 1,
-      name: "TestUser",
-      surname: "Surname",
-      email: "test@test.com",
-      password: "abc",
-      balance: 10000
-    },
-    {
-      userId: 2,
-      name: "TestUser2",
-      surname: "Surnam2e",
-      email: "test@tesdgt.com",
-      password: "abc",
-      balance: 13400
-    },
-    {
-      userId: 3,
-      name: "Tessdg",
-      surname: "osdhg",
-      email: "test@tessdggt.com",
-      password: "abc",
-      balance: 1330
-    },
-    {
-      userId: 4,
-      name: "sdgsdg",
-      surname: "osdhg",
-      email: "test@tessdggt.com",
-      password: "abc",
-      balance: 10
-    },
-    {
-      userId: 5,
-      name: "dsdg",
-      surname: "osdhg",
-      email: "test@tessdggt.com",
-      password: "abc",
-      balance: 10
-    }
+  const { token } = useContext(TokenContext)
 
-  ]
+  const myHeaders = new Headers();
+  myHeaders.append("Authorization", `Bearer ${token}`);
+
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    fetch("https://etazeta.dev/WorkFit/WorkFit/dbif/get_ranked.php", {
+      method: "POST",
+      headers: myHeaders,
+    })
+      .then(response => response.json())
+      .then(result => setData(result))
+      .catch(error => console.log(error));
+  }, []);
 
   return (
     <IonPage>
       <IonContent fullscreen>
         {
-          usersFake.map((user, idx) =>
+          data.map((user, idx) =>
             <LoaderboardUser key={idx} placement={idx + 1}
-              name={user.name}
-              surname={user.surname.charAt(0) + "."}
+              name={user.firstname}
+              surname={user.lastname.charAt(0) + "."}
               balance={user.balance}
             />)
         }
