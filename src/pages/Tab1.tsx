be0ff622a939f2a49493ddf4212eb658a649ea3c
MaFl404 <maurice.fladung22@gmail.com>
2022-10-30 08:04:45 +0100

    Update

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,4 +1,4 @@
-import { IonContent, IonList, IonItem, IonIcon, IonLabel, IonToggle,  IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonList, IonItem, IonIcon, IonLabel, IonToggle, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
 import * as React from 'react'
@@ -6,7 +6,7 @@ import { useState } from 'react';
 import { moon } from "ionicons/icons";
 
 // 1. import `ChakraProvider` component
-import { ChakraProvider, Modal, ColorModeScript, extendTheme, ModalOverlay, useColorMode, useDisclosure, ModalContent, ModalHeader, ModalFooter, ModalBody, ModalCloseButton, FormControl, FormLabel, Input, Wrap, WrapItem, EditablePreview, EditableInput, Avatar, Button, Switch, ButtonGroup, Stack, Alert, Editable, AlertIcon, AlertTitle, AlertDescription, Flex, Box, Text, Badge, ListItem } from '@chakra-ui/react'
+import { ChakraProvider, Accordion, AccordionPanel, AccordionIcon, AccordionButton, AccordionItem, Modal, ColorModeScript, extendTheme, ModalOverlay, useColorMode, useDisclosure, ModalContent, ModalHeader, ModalFooter, ModalBody, ModalCloseButton, FormControl, FormLabel, Input, Wrap, WrapItem, EditablePreview, EditableInput, Avatar, Button, Switch, ButtonGroup, Stack, Alert, Editable, AlertIcon, AlertTitle, AlertDescription, Flex, Box, Text, Badge, ListItem } from '@chakra-ui/react'
 
 
 
@@ -28,7 +28,7 @@ function App() {
       {/* onClick={()=> setShowCode(!showCode)} */}
       <ChakraProvider>
 
-      
+
         <IonPage>
           <IonHeader>
             <IonToolbar>
@@ -41,153 +41,112 @@ function App() {
                 <IonTitle size="large">Tab 1</IonTitle>
               </IonToolbar>
             </IonHeader>
+
+
+
+
+
+
+
+            <Flex display="flex" align='center' justify='center' direction='row' margin='50px'>
+              <Avatar src='' size='xl' />
+              <Box ml='3'>
+                <Text fontWeight='bold' fontSize='25px'>
+                  Account Name
+                </Text>
+                <Text fontSize='20px'>Firma Name</Text>
+              </Box>
+            </Flex>
+            <Flex display="flex" direction='row' align='center' justify='center' margin='20px' >
+
+              <IonIcon slot="start" icon={moon} />
+              <IonLabel>Dark Mode</IonLabel>
+              <IonToggle
+                slot="end"
+                name="darkMode"
+                onIonChange={toggleDarkModeHandler}
+              />
+
+
+
+            </Flex>
+
+            <Flex display="flex" direction='row' align='center' justify='center' >
+              <Button
+                colorScheme='teal'
+                variant='solid'
+                size='md'
+                height='48px'
+                width='300px'
+                border='2px'
+                onClick={onOpen}
+              >
+                Ändern Sie ihren Code
+              </Button>
+              <Modal isOpen={isOpen} onClose={onClose}>
+                <ModalOverlay />
+                <ModalContent>
+                  <ModalHeader></ModalHeader>
+                  <ModalCloseButton />
+                  <ModalBody>
+
+                    <FormControl isRequired width='300px'>
+                      <FormLabel>Bitte geben Sie ihren neuen Code ein</FormLabel>
+                      <Input placeholder='Ihr Code' />
+                    </FormControl>
+
+
+                  </ModalBody>
+                  <ModalFooter>
+                    <Button colorScheme='blue' >
+                      Absenden
+                    </Button>
+                    <Button variant='ghost'>Abbrechen</Button>
+                  </ModalFooter>
+                </ModalContent>
+              </Modal>
+            </Flex>
+
+            <Flex display="flex" direction='row' align='center' justify='center' margin='20px'>
+              <Button
+                colorScheme='teal'
+                variant='ghost'
+                size='md'
+                height='48px'
+                width='300px'
+                border='2px'
+                
+              >
+                E-Mail
+              </Button>
+              
+            </Flex>
+
+            <Flex display="flex" direction='row' align='center' justify='center' margin='20px'>
+              <Button
+                colorScheme='teal'
+                variant='ghost'
+                size='md'
+                height='48px'
+                width='300px'
+                border='2px'
+                
+
+
+              >
+                Passwort
+              </Button>
+
+            </Flex>
+
             
 
-
-      
-     
-    
-
-              <Flex display="flex" align='center' justify='center' direction='row' margin='50px'>
-                <Avatar src='' size='xl'/>
-                <Box ml='3'>
-                  <Text fontWeight='bold' fontSize='25px'>
-                    Account Name
-                  </Text>
-                  <Text fontSize='20px'>Firma Name</Text>
-                </Box>
-              </Flex>
-              <Flex display="flex" direction='row' align='center' justify='center' margin='20px' > 
-              
-            <IonIcon slot="start" icon={moon} />
-            <IonLabel>Dark Mode</IonLabel>
-            <IonToggle
-              slot="end"
-              name="darkMode"
-              onIonChange={toggleDarkModeHandler}
-            />
-          
-        
-      
-              </Flex>
-
-              <Flex display="flex" direction='row' align='center' justify='center' >
-                <Button
-                  colorScheme='teal'
-                  variant='solid'
-                  size='md'
-                  height='48px'
-                  width='300px'
-                  border='2px'
-                  onClick={onOpen}
-                >
-                  Ändern Sie ihren Code
-                </Button>
-                <Modal isOpen={isOpen} onClose={onClose}>
-                  <ModalOverlay />
-                  <ModalContent>
-                    <ModalHeader></ModalHeader>
-                    <ModalCloseButton />
-                    <ModalBody>
-
-                      <FormControl isRequired width='300px'>
-                        <FormLabel>Bitte geben Sie ihren neuen Code ein</FormLabel>
-                        <Input placeholder='Ihr Code' />
-                      </FormControl>
-
-
-                    </ModalBody>
-                    <ModalFooter>
-                      <Button colorScheme='blue' >
-                        Absenden
-                      </Button>
-                      <Button variant='ghost'>Abbrechen</Button>
-                    </ModalFooter>
-                  </ModalContent>
-                </Modal>
-              </Flex>
-
-              <Flex display="flex" direction='row' align='center' justify='center' margin='20px'>
-                <Button
-                  colorScheme='teal'
-                  variant='solid'
-                  size='md'
-                  height='48px'
-                  width='300px'
-                  border='2px'
-                  onClick={onOpen}
-                >
-                  Hier steht ihre E-Mail
-                </Button>
-                <Modal isOpen={isOpen} onClose={onClose}>
-                  <ModalOverlay />
-                  <ModalContent>
-                    <ModalHeader>Ihre E-Mail</ModalHeader>
-                    <ModalCloseButton />
-                    <ModalBody>
-                      <Text>
-                        E-Mail
-                      </Text>
-                     </ModalBody>
-                    <ModalFooter>
-                      <Button colorScheme='blue' onClick={onClose}>
-                        Schließen
-                      </Button>
-                      
-                    </ModalFooter>
-                  </ModalContent>
-                </Modal>
-              </Flex>
-
-
-              <Flex display="flex" direction='row' align='center' justify='center' margin='20px'>
-                <Button
-                  colorScheme='teal'
-                  variant='solid'
-                  size='md'
-                  height='48px'
-                  width='300px'
-                  border='2px'
-                  onClick={onOpen}
-                >
-                  Hier steht ihr Passwort
-                </Button>
-                <Modal isOpen={isOpen} onClose={onClose}>
-                  <ModalOverlay />
-                  <ModalContent>
-                    <ModalHeader>Ihr Passwort</ModalHeader>
-                    <ModalCloseButton />
-                    <ModalBody>
-                      <Text>
-                        Passwort
-                      </Text>
-                     </ModalBody>
-                    <ModalFooter>
-                      <Button colorScheme='blue' onClick={onClose}>
-                        Schließen
-                      </Button>
-                      
-                    </ModalFooter>
-                  </ModalContent>
-                </Modal>
-              </Flex>
-
-              
-
-              
-
-
-
-
-
-
-            
 
           </IonContent>
         </IonPage>
-        </ChakraProvider>
-      </>
-      );
+      </ChakraProvider>
+    </>
+  );
 };
 
-      export default App;
+export default App;
