ec9a6125fa76ea3ca6e73cd6b89b413e869f4355
MaFl404 <maurice.fladung22@gmail.com>
2022-10-30 07:16:32 +0100

    Darkmode hinzugefügt

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,8 +1,9 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonList, IonItem, IonIcon, IonLabel, IonToggle,  IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
 import * as React from 'react'
 import { useState } from 'react';
+import { moon } from "ionicons/icons";
 
 // 1. import `ChakraProvider` component
 import { ChakraProvider, Modal, ColorModeScript, extendTheme, ModalOverlay, useColorMode, useDisclosure, ModalContent, ModalHeader, ModalFooter, ModalBody, ModalCloseButton, FormControl, FormLabel, Input, Wrap, WrapItem, EditablePreview, EditableInput, Avatar, Button, Switch, ButtonGroup, Stack, Alert, Editable, AlertIcon, AlertTitle, AlertDescription, Flex, Box, Text, Badge, ListItem } from '@chakra-ui/react'
@@ -14,6 +15,9 @@ function App() {
   const [showCode, setShowCode] = useState(false)
   const { isOpen, onOpen, onClose } = useDisclosure()
   const { colorMode, toggleColorMode } = useColorMode()
+  const toggleDarkModeHandler = () => {
+    document.body.classList.toggle("dark");
+  };
 
 
   // 2. Wrap ChakraProvider at the root of your app
@@ -22,7 +26,7 @@ function App() {
 
 
       {/* onClick={()=> setShowCode(!showCode)} */}
-
+      <ChakraProvider>
 
       
         <IonPage>
@@ -37,7 +41,12 @@ function App() {
                 <IonTitle size="large">Tab 1</IonTitle>
               </IonToolbar>
             </IonHeader>
-            <ChakraProvider>
+            
+
+
+      
+     
+    
 
               <Flex display="flex" align='center' justify='center' direction='row' margin='50px'>
                 <Avatar src='' size='xl'/>
@@ -48,7 +57,19 @@ function App() {
                   <Text fontSize='20px'>Firma Name</Text>
                 </Box>
               </Flex>
-
+              <Flex display="flex" direction='row' align='center' justify='center' margin='20px' > 
+              
+            <IonIcon slot="start" icon={moon} />
+            <IonLabel>Dark Mode</IonLabel>
+            <IonToggle
+              slot="end"
+              name="darkMode"
+              onIonChange={toggleDarkModeHandler}
+            />
+          
+        
+      
+              </Flex>
 
               <Flex display="flex" direction='row' align='center' justify='center' >
                 <Button
@@ -101,7 +122,7 @@ function App() {
                 <Modal isOpen={isOpen} onClose={onClose}>
                   <ModalOverlay />
                   <ModalContent>
-                    <ModalHeader>Hier ist ihre E-Mail</ModalHeader>
+                    <ModalHeader>Ihre E-Mail</ModalHeader>
                     <ModalCloseButton />
                     <ModalBody>
                       <Text>
@@ -118,6 +139,7 @@ function App() {
                 </Modal>
               </Flex>
 
+
               <Flex display="flex" direction='row' align='center' justify='center' margin='20px'>
                 <Button
                   colorScheme='teal'
@@ -128,12 +150,12 @@ function App() {
                   border='2px'
                   onClick={onOpen}
                 >
-                  Hier steht ihre E-Mail
+                  Hier steht ihr Passwort
                 </Button>
                 <Modal isOpen={isOpen} onClose={onClose}>
                   <ModalOverlay />
                   <ModalContent>
-                    <ModalHeader>Hier ist ihr Passwort</ModalHeader>
+                    <ModalHeader>Ihr Passwort</ModalHeader>
                     <ModalCloseButton />
                     <ModalBody>
                       <Text>
@@ -150,15 +172,20 @@ function App() {
                 </Modal>
               </Flex>
 
+              
+
+              
 
 
 
 
 
-            </ChakraProvider>
+
+            
 
           </IonContent>
         </IonPage>
+        </ChakraProvider>
       </>
       );
 };
