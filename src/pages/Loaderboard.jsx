ee4cfc719b727118790bc8cc1abc6dc9cb6d2d6b
AntonPieper <anton.pieper@informatik.hs-fulda.de>
2022-10-30 07:55:52 +0100

    improves leaderboard

--- src/pages/Loaderboard.jsx
+++ src/pages/Loaderboard.jsx
@@ -1,69 +1,79 @@
+<<<<<<< Updated upstream
 import { IonContent, IonPage } from '@ionic/react';
 import './Home.css';
+=======
+import {IonContent, IonPage} from '@ionic/react';
+>>>>>>> Stashed changes
 import LoaderboardUser from '../components/LeaderBoardUser';
-import { Text } from '@chakra-ui/react';
+import {Divider, Text} from '@chakra-ui/react';
 
 const Loaderboard = () => {
 
-  const usersFake = [
-    {
-      userId: 1,
-      name: "TestUser",
-      surname: "Surname",
-      email: "test@test.com",
-      password: "abc",
-      balance: 10000
-    },
-    {
-      userId: 2,
-      name: "TestUser2",
-      surname: "Surnam2e",
-      email: "test@tesdgt.com",
-      password: "abc",
-      balance: 13400
-    },
-    {
-      userId: 3,
-      name: "Tessdg",
-      surname: "osdhg",
-      email: "test@tessdggt.com",
-      password: "abc",
-      balance: 1330
-    },
-    {
-      userId: 4,
-      name: "sdgsdg",
-      surname: "osdhg",
-      email: "test@tessdggt.com",
-      password: "abc",
-      balance: 10
-    },
-    {
-      userId: 5,
-      name: "dsdg",
-      surname: "osdhg",
-      email: "test@tessdggt.com",
-      password: "abc",
-      balance: 10
-    }
-
-  ]
-
-  return (
-    <IonPage>
-      <IonContent fullscreen>
-        <Text fontSize={"4xl"} as="b" padding={"0.4em"} >Leaderboard</Text>
+    const usersFake = [
         {
-          usersFake.map((user, idx) =>
-            <LoaderboardUser key={idx} placement={idx + 1}
-              name={user.name}
-              surname={user.surname.charAt(0) + "."}
-              balance={user.balance}
-            />)
+            userId: 1,
+            name: "TestUser",
+            surname: "Surname",
+            email: "test@test.com",
+            password: "abc",
+            balance: 10000
+        },
+        {
+            userId: 2,
+            name: "TestUser2",
+            surname: "Surnam2e",
+            email: "test@tesdgt.com",
+            password: "abc",
+            balance: 13400
+        },
+        {
+            userId: 3,
+            name: "Tessdg",
+            surname: "osdhg",
+            email: "test@tessdggt.com",
+            password: "abc",
+            balance: 1330
+        },
+        {
+            userId: 4,
+            name: "sdgsdg",
+            surname: "osdhg",
+            email: "test@tessdggt.com",
+            password: "abc",
+            balance: 10
+        },
+        {
+            userId: 5,
+            name: "dsdg",
+            surname: "osdhg",
+            email: "test@tessdggt.com",
+            password: "abc",
+            balance: 10
         }
-      </IonContent>
-    </IonPage>
-  );
+
+    ]
+
+    return (
+        <IonPage>
+            <IonContent fullscreen>
+                <div style={{paddingInline: ".4em"}}>
+                    <Text fontSize={"4xl"} as="b" paddingBlock={"0.4em"}>Leaderboard</Text>
+                    {
+                        usersFake.map((user, idx, arr) =>
+                            <>
+                                <LoaderboardUser key={idx} placement={idx + 1}
+                                                 name={user.name}
+                                                 surname={user.surname.charAt(0) + "."}
+                                                 balance={user.balance}
+                                />
+                                {idx < arr.length - 1 && <Divider/>}
+                            </>)
+                    }
+                </div>
+
+            </IonContent>
+        </IonPage>
+    );
 };
 
 export default Loaderboard;
