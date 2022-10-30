1db51c84ccd865ba22380eb176a05d47ffebe151
Martin Landsiedel <martin.landsiedel@continental-corporation.com>
2022-10-30 05:50:30 +0100

    Fix error in register; Add jwt_valid function
    Validate token with jwt_valid.php

---
+++ dbif/jwt_valid.php
@@ -0,0 +1,32 @@
+<?php
+
+// Check if token is still valid
+// Return: 'valid':false if not valid. Else true
+
+/* Connect to database
+$db_host_name = 'db5010643227.hosting-data.io';
+$database = 'dbs9005828';
+$db_user_name = 'dbu1417295';
+$db_password = 'C5YVJVudH55u';
+$pdo = new PDO("mysql:host=$db_host_name;dbname=$database",
+                $db_user_name,
+                $db_password);
+*/
+
+require_once 'jwt_utils.php';                               // JWT Authentication
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Allow-Headers: Authorization");
+
+$bearer_token = get_bearer_token();
+
+$is_jwt_valid = is_jwt_valid($bearer_token);
+if(!$is_jwt_valid) {
+	die(json_encode(array('valid' => false)));
+}
+else {
+    die(json_encode(array('valid' => true)));
+}
+                
+?>
